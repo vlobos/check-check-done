e1664e4883b23f78c5d221cbbbd0cd4852b1f6a3
vlobos <suryvlobos@gmail.com>
2018-12-18 10:26:55 -0800

    serving static files

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,37 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-// lib/app.ts
-const express = require("express");
-// Create a new express application instance
-const app = express();
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!');
-});
+const express_1 = __importDefault(require("express"));
+const bodyParser = __importStar(require("body-parser"));
+const path = __importStar(require("path"));
+//server class
+class Server {
+    constructor() {
+        //create expressjs application
+        this.app = express_1.default();
+        //configure application
+        this.config();
+    }
+    config() {
+        this.app.use(bodyParser.urlencoded({ extended: true }));
+        this.app.use(bodyParser.json());
+        this.app.get("/", (req, res) => {
+            res.sendFile(path.join(__dirname, "../public", "index.html"));
+        });
+        this.app.use(express_1.default.static(path.resolve(__dirname, "../public")));
+        this.app.listen(8080, () => {
+            console.log("Listening on Port 8080.");
+        });
+    }
+}
+exports.default = new Server().app;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,31 @@
-// lib/app.ts
-import express = require('express');
+import express from "express";
+import * as bodyParser from "body-parser";
+import * as path from "path";
 
-// Create a new express application instance
-const app: express.Application = express();
+//server class
+class Server {
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+  public app: express.Application;
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
-});
+  constructor(){
+    //create expressjs application
+    this.app = express();
+    //configure application
+    this.config();
+  }
+
+  public config(): void {
+    this.app.use(bodyParser.urlencoded({ extended: true }));
+    this.app.use(bodyParser.json());
+    this.app.get("/", (req,res)=>{
+      res.sendFile(path.join(__dirname, "../public","index.html"))
+    });
+    this.app.use(express.static(path.resolve(__dirname, "../public")));
+
+    this.app.listen(8080, ()=>{
+      console.log("Listening on Port 8080.")
+    })
+  }
+}
+
+export default new Server().app;
